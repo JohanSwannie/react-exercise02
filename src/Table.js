49ad7c52891008f4d97e5cdca3faa3b6d4fb8c9a
jswan <jswannieman@gmail.com>
2023-03-15 10:26:39 +1100

    Update Table Javascript Component

--- src/Table.js
+++ src/Table.js
@@ -1,9 +1,9 @@
 import Row from "./Row";
 
-const Table = ({ items }) => {
+const Table = ({ items, reqType }) => {
   return (
     <div className="table-container">
-      <p>Table Display</p>
+      <p>Table Items for {reqType}</p>
       <table>
         <tbody>
           {items.map((item) => (
