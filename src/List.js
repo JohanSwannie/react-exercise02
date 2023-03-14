b2be3d83b9f9e145a00d3d6d399006aab641d732
jswan <jswannieman@gmail.com>
2023-03-15 08:40:15 +1100

    Update List Javascript  Component

--- src/List.js
+++ src/List.js
@@ -3,6 +3,7 @@ import ListItem from "./ListItem";
 const List = ({ items }) => {
   return (
     <ul>
+      <p>List Display - Scroll down for Table Display</p>
       {items.map((item) => (
         <ListItem key={item.id} item={item} />
       ))}
